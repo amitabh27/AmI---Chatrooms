369a6127ea8f26374b74f031383ab65d254742fb
Amitabh tiwari <amitabh.tiwari27@gmail.com>
2017-09-01 19:14:29 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,19 +1,19 @@
 module.exports={
 'facebookAuth' : {
-		'clientID':'511633009215146',
-		'clientSecret':'0a772bab99b4875ba23be17f56c8806f',
+		'clientID':'enter',
+		'clientSecret':'enter',
 		'callbackURL':'http://localhost:8080/auth/facebook/callback'
 
 	},
 'twitterAuth' : {
-        'consumerKey'        : 'rZby0710VMjvcHfYJqvw1ORkg',
-        'consumerSecret'     : 'jez9wmk9zi5xtlcxXzDz2OKFOPx3O6OjHs9pq3wRuDwKZjIM8Z',
+        'consumerKey'        : 'enter',
+        'consumerSecret'     : 'enter',
         'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
     },
 
 	'googleAuth' : {
-		'clientID': '541064440059-3jqs117i3s7nd28mrn80a5gbmegvidq7.apps.googleusercontent.com',
-		'clientSecret': 'umdebFDCDsm4YyEV_YW5lYVB',
+		'clientID': 'enter',
+		'clientSecret': 'enter',
 		'callbackURL': 'http://localhost:8080/auth/google/callback'
 	}
 }
